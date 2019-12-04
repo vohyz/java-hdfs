40ba7d922d0c2dc9e45549cb9e5617f8459ca049
vohyz <531322336@qq.com>
2019-12-04 13:24:51 +0800

    feature - 第一部分完成

--- hw2/MTWriter.java
+++ hw2/MTWriter.java
@@ -3,27 +3,29 @@ package hw2;
 import hw2.Producer;
 import java.nio.file.*;
 import java.nio.CharBuffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel.*;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
 import java.nio.MappedByteBuffer;
 import java.nio.charset.Charset;
 import java.io.IOException;
 public class MTWriter {
     public static void main(String [] args) {
-        Path path = Paths.get("output.txt");
-        String number = Producer.create();
         long startTime = System.currentTimeMillis();    //获取开始时间
-        CharBuffer charBuffer = CharBuffer.wrap(number);
+        Path path = Paths.get("output.txt");
+        byte[] number = Producer.create();
+        ByteBuffer byteBuffer = ByteBuffer.wrap(number);
         try (FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.READ, StandardOpenOption.WRITE,
                 StandardOpenOption.TRUNCATE_EXISTING)) {
-            MappedByteBuffer mappedByteBuffer = fileChannel.map(MapMode.READ_WRITE, 0, 2014*512*256);
+            MappedByteBuffer mappedByteBuffer = fileChannel.map(MapMode.READ_WRITE, 0, 2014*512*256*4);
             if (mappedByteBuffer != null) {
-                mappedByteBuffer.put(Charset.forName("UTF-8").encode(charBuffer));
+                mappedByteBuffer.put(byteBuffer);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
         long endTime = System.currentTimeMillis();    //获取结束时间
         System.out.println("程序运行时间：" + (endTime - startTime) + "ms");    //输出程序运行时间
     }
--- hw2/Producer.java
+++ hw2/Producer.java
@@ -1,14 +1,25 @@
 package hw2;
 
 public class Producer {
-    public static String create() {
-        String number = "";
+    public static byte[] create() {
+        byte[] number = new byte[2014*512*256*4];
         for(int j = 0;j < 2014*512;j++) {
-            for(int i = 0;i < 256;i ++) {
-                String str = String.valueOf(j);
-                number += str;
+            for(int i = 0;i < 256;i += 4) {
+                number[i+256*j] = int2Byte(j)[0];
+                number[i+1+256*j] = int2Byte(j)[1];
+                number[i+2+256*j] = int2Byte(j)[2];
+                number[i+3+256*j] = int2Byte(j)[3];
             }
         }
         return number;
+    }
+
+    public static byte[] int2Byte(int i) {  
+        byte[] result = new byte[4];  
+        result[0] = (byte)((i >> 24) & 0xFF);
+        result[1] = (byte)((i >> 16) & 0xFF);
+        result[2] = (byte)((i >> 8) & 0xFF);
+        result[3] = (byte)(i & 0xFF);
+        return result;
     }
 }
